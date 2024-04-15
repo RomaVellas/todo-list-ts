61fd7bcc3b41776e0a3ad481b55c579955eeaac7
RomaVellas <zebomenn@bk.ru>
2024-04-15 20:15:09 +0300

    add move to global

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import TodoList from '../TodoList/TodoList';
+import TodoList from '../components/TodoList/TodoList';
 import './App.css';
 
 const App: FC = () => {
